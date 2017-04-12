c7c82b2f25aecabc04d5b9b6a184db2264f85348
touch head optimistically <741589196@qq.com>
2017-04-12 16:43:48 +0800

    Add files via upload

---
+++ src/main/Log.scala
@@ -0,0 +1,43 @@
+package main
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+/**
+  * Created by yang.rui on 2017.
+  *
+  * Log.scala
+  *
+  * log
+  *
+  *
+  */
+object Log{
+
+  def info(message :String): Unit ={
+
+    val dateFormatter = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss")
+    var nowtime = new Date()
+    var now = dateFormatter.format(nowtime)
+
+    println("[INFO]" + " [" + now + "] " + message)
+  }
+
+  def warn(message: String): Unit ={
+
+    val dateFormatter = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss")
+    var nowtime = new Date()
+    var now = dateFormatter.format(nowtime)
+
+    println("[WARN]" + " [" + now + "] " + message)
+  }
+
+  def error(message: String): Unit = {
+
+    val dateFormatter = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss")
+    var nowtime = new Date()
+    var now = dateFormatter.format(nowtime)
+
+    println("[ERROR]" + "[" + now + "] " + message)
+  }
+}
